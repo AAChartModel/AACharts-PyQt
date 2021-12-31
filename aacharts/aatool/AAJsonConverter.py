7e9e2d7816fca0c0105d70e708cb48e81058dbe1
An An <2236368544@qq.com>
2021-12-31 17:36:26 +0800

    Update AAJsonConverter.py

--- aacharts/aatool/AAJsonConverter.py
+++ aacharts/aatool/AAJsonConverter.py
@@ -13,5 +13,23 @@ class AAJsonConverter:
     def convertObjectToPureJson(obj: object):
         jsonStr = AAJsonConverter.convertObjectToJson(obj)
         jsonDic = json.loads(s=jsonStr)
-        prettyJsonStr = json.dumps(jsonDic, sort_keys=True, indent=2, separators=(',', ':'), ensure_ascii=False)
+        pureJsonDic = AAJsonConverter.del_none(jsonDic)
+        prettyJsonStr = json.dumps(pureJsonDic, sort_keys=True, indent=2, separators=(',', ':'), ensure_ascii=False)
         return prettyJsonStr
+
+    # https://stackoverflow.com/questions/4255400/exclude-empty-null-values-from-json-serialization
+    @staticmethod
+    def del_none(d):
+        """
+        Delete keys with the value ``None`` in a dictionary, recursively.
+
+        This alters the input so you may wish to ``copy`` the dict first.
+        """
+        # For Python 3, write `list(d.items())`; `d.items()` won’t work
+        # For Python 2, write `d.items()`; `d.iteritems()` won’t work
+        for key, value in list(d.items()):
+            if value is None:
+                del d[key]
+            elif isinstance(value, dict):
+                AAJsonConverter.del_none(value)
+        return d
