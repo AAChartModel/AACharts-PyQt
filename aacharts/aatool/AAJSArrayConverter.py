ec681eb1f25e6d06254d0a3496b55e39553ac13b
An An <2236368544@qq.com>
2021-12-31 14:40:10 +0800

    Update AAJSArrayConverter.py

--- aacharts/aatool/AAJSArrayConverter.py
+++ aacharts/aatool/AAJSArrayConverter.py
@@ -4,6 +4,6 @@ class AAJSArrayConverter:
     def JSArrayWithHaxeArray(arr):
         jsArrStr = ""
         for element in arr:
-            jsArrStr = jsArrStr + f"'${element}',"
+            jsArrStr = jsArrStr + f"'{element}',"
 
-        return f"[${jsArrStr}]"
+        return f"[{jsArrStr}]"
